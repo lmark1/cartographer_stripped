5b9a2164e8ade6442f42114f0ea28d75e33a6a6c
Lovro Markovic <lovro.markovic@fer.hr>
2021-03-23 16:07:38 +0100

    Add 'uav_ros_lib' dependancy, load parameters with lua files

---
+++ lua_config/trajectory_builder_3d_options.lua
@@ -0,0 +1,63 @@
+TRAJECTORY_BUILDER_3D_OPTIONS = {
+  min_range = 0.5,
+  max_range = 50.,
+  num_accumulated_range_data = 1,
+  voxel_filter_size = 0.2,
+
+  high_resolution_adaptive_voxel_filter = {
+    max_length = 0.7,
+    min_num_points = 200,
+    max_range = 50.,
+  },
+
+  low_resolution_adaptive_voxel_filter = {
+    max_length = 0.7,
+    min_num_points = 200,
+    max_range = 50.,
+  },
+
+  use_online_correlative_scan_matching = false,
+  real_time_correlative_scan_matcher = {
+    linear_search_window = 0.2,
+    angular_search_window = math.rad(1.),
+    translation_delta_cost_weight = 1e-1,
+    rotation_delta_cost_weight = 1.,
+  },
+
+  ceres_scan_matcher = {
+    occupied_space_weight_0 = 5.,
+    occupied_space_weight_1 = 20.,
+    translation_weight = 0.1,
+    translation_weight_z = 0.1,
+    rotation_weight = 0.3,
+    only_optimize_yaw = false,
+    ceres_solver_options = {
+      use_nonmonotonic_steps = true,
+      max_num_iterations = 20,
+      num_threads = 1,
+    },
+  },
+
+  motion_filter = {
+    max_time_seconds = 0.2,
+    max_distance_meters = 0.02,
+    max_angle_radians = 0.001,
+  },
+
+  imu_gravity_time_constant = 1.,
+  rotational_histogram_size = 120,
+
+  submaps = {
+    high_resolution = 0.2,
+    high_resolution_max_range = 50.,
+    low_resolution = 0.5,
+    num_range_data = 45000,
+    range_data_inserter = {
+      hit_probability = 0.7,
+      miss_probability = 0.4,
+      num_free_space_voxels = 0,
+    },
+  },
+}
+
+return TRAJECTORY_BUILDER_3D_OPTIONS
--- nodes/local_trajectory_builder_3d_node.cpp
+++ nodes/local_trajectory_builder_3d_node.cpp
@@ -1,9 +1,11 @@
+#include <cartographer_stripped/common/configuration_file_resolver.h>
 #include <cartographer_stripped/common/lua_parameter_dictionary_test_helpers.h>
 #include <cartographer_stripped/common/msg_conversion.h>
 #include <cartographer_stripped/common/tf_bridge.h>
 #include <cartographer_stripped/common/time_conversion.h>
 #include <cartographer_stripped/mapping/local_trajectory_builder_3d.h>
 #include <cartographer_stripped/mapping/local_trajectory_builder_options_3d.h>
+#include <uav_ros_lib/param_util.hpp>
 
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
@@ -25,73 +27,17 @@ const std::string& CheckNoLeadingSlash(const std::string& frame_id) {
 }
 
 cartographer_stripped::mapping::proto::LocalTrajectoryBuilderOptions3D
-CreateTrajectoryBuilderOptions3D() {
-  auto parameter_dictionary =
-      cartographer_stripped::common::MakeDictionary(R"text(
-        return {
-          min_range = 0.5,
-          max_range = 50.,
-          num_accumulated_range_data = 1,
-          voxel_filter_size = 0.2,
-
-          high_resolution_adaptive_voxel_filter = {
-            max_length = 0.7,
-            min_num_points = 200,
-            max_range = 50.,
-          },
-
-          low_resolution_adaptive_voxel_filter = {
-            max_length = 0.7,
-            min_num_points = 200,
-            max_range = 50.,
-          },
-
-          use_online_correlative_scan_matching = false,
-          real_time_correlative_scan_matcher = {
-            linear_search_window = 0.2,
-            angular_search_window = math.rad(1.),
-            translation_delta_cost_weight = 1e-1,
-            rotation_delta_cost_weight = 1.,
-          },
-
-          ceres_scan_matcher = {
-            occupied_space_weight_0 = 5.,
-            occupied_space_weight_1 = 20.,
-            translation_weight = 0.1,
-            translation_weight_z = 0.1,
-            rotation_weight = 0.3,
-            only_optimize_yaw = false,
-            ceres_solver_options = {
-              use_nonmonotonic_steps = true,
-              max_num_iterations = 20,
-              num_threads = 1,
-            },
-          },
-
-          motion_filter = {
-            max_time_seconds = 0.2,
-            max_distance_meters = 0.02,
-            max_angle_radians = 0.001,
-          },
-
-          imu_gravity_time_constant = 1.,
-          rotational_histogram_size = 120,
-
-          submaps = {
-            high_resolution = 0.2,
-            high_resolution_max_range = 50.,
-            low_resolution = 0.5,
-            num_range_data = 45000,
-            range_data_inserter = {
-              hit_probability = 0.7,
-              miss_probability = 0.4,
-              num_free_space_voxels = 0,
-            },
-          },
-        }
-        )text");
+CreateTrajectoryBuilderOptions3D(const std::string& configuration_directory,
+                                 const std::string& configuration_basename) {
+  auto file_resolver = std::make_unique<
+      cartographer_stripped::common::ConfigurationFileResolver>(
+      std::vector<std::string>{configuration_directory});
+  const std::string code =
+      file_resolver->GetFileContentOrDie(configuration_basename);
+  cartographer_stripped::common::LuaParameterDictionary
+      lua_parameter_dictionary(code, std::move(file_resolver));
   return cartographer_stripped::mapping::CreateLocalTrajectoryBuilderOptions3D(
-      parameter_dictionary.get());
+      &lua_parameter_dictionary);
 }
 
 void imu_callback(const sensor_msgs::ImuConstPtr& msg) {
@@ -187,9 +133,18 @@ sensor_msgs::PointCloud2Ptr get_submap() {
 int main(int argc, char** argv) {
   ros::init(argc, argv, "local_trajectory_builder_node");
 
+  ros::NodeHandle nh_private("~");
+  std::string configuration_basename, configuration_directory;
+  param_util::getParamOrThrow(nh_private, "configuration_basename",
+                              configuration_basename);
+  param_util::getParamOrThrow(nh_private, "configuration_directory",
+                              configuration_directory);
+
   trajectory_builder_ptr = std::make_unique<
       cartographer_stripped::mapping::LocalTrajectoryBuilder3D>(
-      CreateTrajectoryBuilderOptions3D(), std::vector<std::string>{"lidar"});
+      CreateTrajectoryBuilderOptions3D(configuration_directory,
+                                       configuration_basename),
+      std::vector<std::string>{"lidar"});
 
   tf2_ros::Buffer tf2_buffer;
   tf2_ros::TransformListener tf(tf2_buffer);
