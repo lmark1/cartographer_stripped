6c0d5d4413c5bc4fb6cae38d0a68f766b2881deb
Lovro Markovic <lovro.markovic@fer.hr>
2021-03-25 14:20:40 +0100

    Load local_trajectory_builder_3d as a plugin in local_trajectory_manager nodelet.

--- nodes/local_trajectory_manager.cpp
+++ nodes/local_trajectory_manager.cpp
@@ -1,20 +1,18 @@
-#include <cartographer_stripped/common/configuration_file_resolver.h>
-#include <cartographer_stripped/common/lua_parameter_dictionary_test_helpers.h>
-#include <cartographer_stripped/common/msg_conversion.h>
-#include <cartographer_stripped/common/tf_bridge.h>
-#include <cartographer_stripped/common/time_conversion.h>
-#include <cartographer_stripped/mapping/local_trajectory_builder_3d.h>
-#include <cartographer_stripped/mapping/local_trajectory_builder_options_3d.h>
+#include <cartographer_stripped/plugins/trajectory_builder_interface.h>
 #include <uav_ros_lib/param_util.hpp>
 
 #include <nav_msgs/Odometry.h>
 #include <nodelet/nodelet.h>
+#include <pluginlib/class_loader.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <tf2_ros/transform_listener.h>
 
+using iBuilder_t = cartographer_stripped::mapping::trajectory_builder_interface;
+
 namespace cartographer_stripped {
+
 
 /**
  * @brief Local Trajectory Builder nodelet.
@@ -25,24 +23,61 @@ class LocalTrajectoryManager : public nodelet::Nodelet {
   void onInit() override;
 
  private:
-  // Trajectory builder resources
-  std::unique_ptr<mapping::LocalTrajectoryBuilder3D> m_trajectory_builder_ptr;
-  std::unique_ptr<TfBridge> m_tf_bridge_ptr;
+  // Check if nodelet is initialized
+  bool m_is_initialized = false;
 
-  // Default frame name variables
-  std::string m_tracking_frame{"tracking_frame"};
-  std::string m_map_frame{"map_frame"};
-  std::string m_published_frame{"published_frame"};
-  std::string m_odom_frame{"odom_frame"};
-  std::string m_lidar_frame{"lidar_frame"};
+  // Trajectory builder class loader
+  std::unique_ptr<pluginlib::ClassLoader<iBuilder_t>> m_trajectory_builder_loader;
+  boost::shared_ptr<iBuilder_t>                       m_trajectory_builder_ptr;
 };
 
 void LocalTrajectoryManager::onInit() {
-  ROS_INFO("[LocalTrajectoryManager] Hello from onInit()!");
+  ROS_INFO("[LocalTrajectoryManager] onInit()!");
+
+  // Load Private parameters
+  auto& nh_private = getMTPrivateNodeHandle();
+  auto& nh         = getMTNodeHandle();
+
+  ros::Time::waitForValid();
+
+  // Initialize the trajectory loader
+  m_trajectory_builder_loader = std::make_unique<pluginlib::ClassLoader<iBuilder_t>>(
+      "cartographer_stripped",
+      "cartographer_stripped::mapping::trajectory_builder_interface");
+
+  // Get the builder adress
+  std::string builder_adress;
+  param_util::getParamOrThrow(nh_private, "trajectory_builder_adress", builder_adress);
+
+  // Load the plugin
+  try {
+    m_trajectory_builder_ptr =
+        m_trajectory_builder_loader->createInstance(builder_adress);
+  } catch (pluginlib::CreateClassException& ex1) {
+    ROS_ERROR("[LocalTrajectoryManager]: CreateClassException '%s'",
+              builder_adress.c_str());
+    ROS_ERROR("[LocalTrajectoryManager]: Error: %s", ex1.what());
+    ros::shutdown();
+  } catch (pluginlib::PluginlibException& ex) {
+    ROS_ERROR("[LocalTrajectoryManager]: PluginlibException '%s'",
+              builder_adress.c_str());
+    ROS_ERROR("[LocalTrajectoryManager]: Error: %s", ex.what());
+    ros::shutdown();
+  }
+
+  // Initialize the plugin
+  auto [success, message] = m_trajectory_builder_ptr->initialize(nh, nh_private);
+  if (!success) {
+    ROS_FATAL(message.c_str());
+    ROS_FATAL("[LocalTrajectoryManager] Failed to load the %s plugin.",
+              builder_adress.c_str());
+    ros::shutdown();
+  }
+
+  m_is_initialized = true;
 }
 
 }  // namespace cartographer_stripped
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(cartographer_stripped::LocalTrajectoryManager,
-                       nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(cartographer_stripped::LocalTrajectoryManager, nodelet::Nodelet)
--- src/plugins/local_trajectory_builder_3d_plugin.cpp
+++ src/plugins/local_trajectory_builder_3d_plugin.cpp
@@ -1,20 +1,57 @@
+#include <cartographer_stripped/common/configuration_file_resolver.h>
+#include <cartographer_stripped/common/lua_parameter_dictionary_test_helpers.h>
+#include <cartographer_stripped/common/msg_conversion.h>
+#include <cartographer_stripped/common/tf_bridge.h>
+#include <cartographer_stripped/common/time_conversion.h>
+#include <cartographer_stripped/mapping/local_trajectory_builder_3d.h>
+#include <cartographer_stripped/mapping/local_trajectory_builder_options_3d.h>
 #include <cartographer_stripped/plugins/trajectory_builder_interface.h>
+#include <uav_ros_lib/param_util.hpp>
 
 namespace cartographer_stripped {
 namespace mapping {
 class LocalTrajectoryBuilder3DPlugin : public trajectory_builder_interface {
  public:
   LocalTrajectoryBuilder3DPlugin();
-  std::tuple<bool, std::string> initialize(
-      ros::NodeHandle& nh, ros::NodeHandle& nh_private) override;
+  std::tuple<bool, std::string> initialize(ros::NodeHandle& nh,
+                                           ros::NodeHandle& nh_private) override;
+
   void add_imu_data(sensor_msgs::ImuConstPtr& imu_msg) override;
   void add_odometry_data(nav_msgs::OdometryConstPtr& odom_msg) override;
-  void add_pointcloud2_data(
-      sensor_msgs::PointCloud2ConstPtr& cloud_msg) override;
+  void add_pointcloud2_data(sensor_msgs::PointCloud2ConstPtr& cloud_msg) override;
+
   sensor_msgs::PointCloud2Ptr get_map() override;
 
  private:
+  /**
+   * @brief Create a trajectory builder options object
+   *
+   * @param configuration_directory
+   * @param configuration_basename
+   * @return
+   * cartographer_stripped::mapping::proto::LocalTrajectoryBuilderOptions3D
+   */
+  cartographer_stripped::mapping::proto::LocalTrajectoryBuilderOptions3D
+  create_trajectory_builder_options(const std::string& configuration_directory,
+                                    const std::string& configuration_basename);
+
+  // Check if the plugin is initialized
   bool m_is_initialize = false;
+
+  // Default frame name variables
+  std::string m_tracking_frame  = "default_tracking_frame";
+  std::string m_map_frame       = "default_map_frame";
+  std::string m_published_frame = "default_published_frame";
+  std::string m_odom_frame      = "default_odom_frame";
+  std::string m_lidar_frame     = "default_lidar_frame";
+
+  // Configuration parameters
+  std::string m_configuration_basename  = "default_config.lua";
+  std::string m_configuration_directory = "path/to/config";
+
+  // Trajectory builder resources
+  std::unique_ptr<LocalTrajectoryBuilder3D> m_trajectory_builder_ptr;
+  std::unique_ptr<TfBridge>                 m_tf_bridge_ptr;
 };
 
 LocalTrajectoryBuilder3DPlugin::LocalTrajectoryBuilder3DPlugin() {
@@ -24,12 +61,19 @@ LocalTrajectoryBuilder3DPlugin::LocalTrajectoryBuilder3DPlugin() {
 std::tuple<bool, std::string> LocalTrajectoryBuilder3DPlugin::initialize(
     ros::NodeHandle& nh, ros::NodeHandle& nh_private) {
   ROS_INFO("[LocalTrajectoryBuilder3DPlugin] Initialize");
+  param_util::getParamOrThrow(nh_private, "configuration_basename",
+                              m_configuration_basename);
+  param_util::getParamOrThrow(nh_private, "configuration_directory",
+                              m_configuration_directory);
   m_is_initialize = true;
   return {true, "LocalTrajectoryBuilder3DPlugin initialized succesfully."};
 }
 
-void LocalTrajectoryBuilder3DPlugin::add_imu_data(
-    sensor_msgs::ImuConstPtr& imu_msg) {
+void LocalTrajectoryBuilder3DPlugin::add_imu_data(sensor_msgs::ImuConstPtr& imu_msg) {
+  if (!m_is_initialize) {
+    return;
+  }
+
   ROS_INFO_THROTTLE(5.0, "[LocalTrajectoryBuilder3DPlugin] add_imu_data");
 }
 
@@ -40,13 +84,34 @@ void LocalTrajectoryBuilder3DPlugin::add_odometry_data(
 
 void LocalTrajectoryBuilder3DPlugin::add_pointcloud2_data(
     sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
-  ROS_INFO_THROTTLE(5.0,
-                    "[LocalTrajectoryBuilder3DPlugin] add_pointcloud2_data");
+  ROS_INFO_THROTTLE(5.0, "[LocalTrajectoryBuilder3DPlugin] add_pointcloud2_data");
 }
 
 sensor_msgs::PointCloud2Ptr LocalTrajectoryBuilder3DPlugin::get_map() {
   ROS_INFO_THROTTLE(5.0, "[LocalTrajectoryBuilder3DPlugin] get_map");
 }
+
+cartographer_stripped::mapping::proto::LocalTrajectoryBuilderOptions3D
+LocalTrajectoryBuilder3DPlugin::create_trajectory_builder_options(
+    const std::string& configuration_directory,
+    const std::string& configuration_basename) {
+  auto file_resolver =
+      std::make_unique<cartographer_stripped::common::ConfigurationFileResolver>(
+          std::vector<std::string>{configuration_directory});
+  const std::string code = file_resolver->GetFileContentOrDie(configuration_basename);
+  cartographer_stripped::common::LuaParameterDictionary lua_parameter_dictionary(
+      code, std::move(file_resolver));
+
+  m_tracking_frame  = lua_parameter_dictionary.GetString("tracking_frame");
+  m_map_frame       = lua_parameter_dictionary.GetString("map_frame");
+  m_published_frame = lua_parameter_dictionary.GetString("published_frame");
+  m_odom_frame      = lua_parameter_dictionary.GetString("odom_frame");
+  m_lidar_frame     = lua_parameter_dictionary.GetString("lidar_frame");
+
+  return cartographer_stripped::mapping::CreateLocalTrajectoryBuilderOptions3D(
+      lua_parameter_dictionary.GetDictionary("trajectory_builder_3d_options").get());
+}
+
 }  // namespace mapping
 }  // namespace cartographer_stripped
 
