d15ec234ae9f8e4a65d058dca356612c0b6fb76f
Lovro Markovic <lovro.markovic@fer.hr>
2021-04-01 12:57:50 +0200

    [CeresScanMatcher3D] Formatting

--- include/cartographer_stripped/mapping/scan_matching/ceres_scan_matcher_3d.h
+++ include/cartographer_stripped/mapping/scan_matching/ceres_scan_matcher_3d.h
@@ -33,14 +33,15 @@ namespace mapping {
 namespace scan_matching {
 
 proto::CeresScanMatcherOptions3D CreateCeresScanMatcherOptions3D(
-    common::LuaParameterDictionary* parameter_dictionary);
+  common::LuaParameterDictionary* parameter_dictionary);
 
 using PointCloudAndHybridGridPointers =
-    std::pair<const sensor::PointCloud*, const HybridGrid*>;
+  std::pair<const sensor::PointCloud*, const HybridGrid*>;
 
 // This scan matcher uses Ceres to align scans with an existing map.
-class CeresScanMatcher3D {
- public:
+class CeresScanMatcher3D
+{
+public:
   explicit CeresScanMatcher3D(const proto::CeresScanMatcherOptions3D& options);
 
   CeresScanMatcher3D(const CeresScanMatcher3D&) = delete;
@@ -49,20 +50,20 @@ class CeresScanMatcher3D {
   // Aligns 'point_clouds' within the 'hybrid_grids' given an
   // 'initial_pose_estimate' and returns a 'pose_estimate' and the solver
   // 'summary'.
-  void Match(const Eigen::Vector3d& target_translation,
-             const transform::Rigid3d& initial_pose_estimate,
-             const std::vector<PointCloudAndHybridGridPointers>&
-                 point_clouds_and_hybrid_grids,
-             transform::Rigid3d* pose_estimate,
-             ceres::Solver::Summary* summary);
+  void Match(
+    const Eigen::Vector3d&                              target_translation,
+    const transform::Rigid3d&                           initial_pose_estimate,
+    const std::vector<PointCloudAndHybridGridPointers>& point_clouds_and_hybrid_grids,
+    transform::Rigid3d*                                 pose_estimate,
+    ceres::Solver::Summary*                             summary);
 
- private:
+private:
   const proto::CeresScanMatcherOptions3D options_;
-  ceres::Solver::Options ceres_solver_options_;
+  ceres::Solver::Options                 ceres_solver_options_;
 };
 
-}  // namespace scan_matching
-}  // namespace mapping
-}  // namespace cartographer_stripped
+}// namespace scan_matching
+}// namespace mapping
+}// namespace cartographer_stripped
 
-#endif  // CARTOGRAPHER_MAPPING_INTERNAL_3D_SCAN_MATCHING_CERES_SCAN_MATCHER_3D_H_
+#endif// CARTOGRAPHER_MAPPING_INTERNAL_3D_SCAN_MATCHING_CERES_SCAN_MATCHER_3D_H_
